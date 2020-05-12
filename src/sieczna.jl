e6a5e4a1953c939dcb25fcec54b3f08de266835d
mikolajlisicki <steamkontopg@wp.pl>
2020-05-12 23:38:08 +0200

    Stworzenie repozytorium i metoda

---
+++ src/sieczna.jl
@@ -0,0 +1,20 @@
+module sieczna
+
+function secant(f::Function,pzero,pone,eps,N)
+		n=1
+		p=0.
+		while n<=N
+			p=pone-f(pone)*(pone-pzero)/(f(pone)-f(pzero))
+			if f(p)==0 || abs(p-pone)<eps
+				return p
+			end
+			pzero=pone
+			pone=p
+			n=n+1
+		end
+		y=f(p)
+		println("Nie udało się. Ostatnia iteracja dala nam $p z y=")
+	end
+export secant
+
+end # module
